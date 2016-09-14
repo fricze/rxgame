ecc7adca5331918b83a27bd54cd903a2b4745fd0
Andrzej <andrzej.fricze@codete.co>
2016-09-14 20:19:26 +0200

    Create proper dependency between data and events

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ import getCharFromKeyCode from './keycodes';
 import { isValue } from './fn';
 import { exampleString } from './data';
 
-const stringStream = Array.from(exampleString);
+const currentString = Array.from(exampleString);
 
-const keyUpStream = Rx.Observable
-        .fromEvent(window, 'keyup')
-        .map(({keyCode}) => keyCode)
-        .map(keyCode => getCharFromKeyCode(keyCode))
-        .filter(isValue)
-        .filter(key => key === stringStream[0])
-        .subscribe(key => {
-          stringStream.shift();
+const keyUpStream = Rx.Observable.fromEvent(window, 'keyup');
 
-          console.log(stringStream);
-        });
+const dataStream = new Rx.Subject();
+
+keyUpStream
+  .map(({keyCode}) => keyCode)
+  .map(keyCode => getCharFromKeyCode(keyCode))
+  .filter(isValue)
+  .filter(key => key === currentString[0])
+  .withLatestFrom(dataStream, (key, data) => ({key, data}))
+  .subscribe(({key, data}) => {
+    data.shift();
+    dataStream.onNext(data)
+
+    console.log(data);
+  });
+
+dataStream.onNext(currentString);

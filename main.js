476ef76a327af17a5dc1c5f41a99e2258e3fbcc5
Webdwarves <contact@webdwarves.com>
2016-09-17 17:07:35 +0200

    funny stuff ha ha ha

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
 import Rx from 'rx';
 import getCharFromKeyCode from './keycodes';
 import { isValue } from './fn';
-import { exampleString } from './data';
 
-const sourceCharArr = Rx.Observable.return(exampleString);
-const shiftCharArr = {
-  next: charArr => {
-    charArr.shift();
-    return charArr;
+const l = console.log;
+const observer1 = {
+  next: (v) => console.log(v),
+  complete: () => 'completed'
+}
+var observer2 = Rx.Observer.create(
+  function (x) {
+    console.log('Next: ' + x);
   },
-  error: err => console.error('Observer got an error: ' + err),
-  complete: () => console.log('Observer got a complete notification')
-};
+  function (err) {
+    console.log('Error: ' + err);
+  },
+  function () {
+    console.log('Completed');
+  }
+);
 
-sourceCharArr.subscribe(shiftCharArr);
+let sourceCharArr;
+let sourceArrObservable
+const fromKeyBoard$ =  Rx.Observable.fromEvent(window, 'keyup');
 
-const keyUpStream = Rx.Observable.fromEvent(window, 'keyup');
-
-keyUpStream
+const source  = fromKeyBoard$
   .map(({keyCode}) => keyCode)
   .map(keyCode => getCharFromKeyCode(keyCode))
-  .filter(isValue)
-  .withLatestFrom(sourceCharArr, (key, data) => ({key, data}))
-  .filter(({key, data}) => key === data[0])
-  .subscribe(({data}) => {
-    source.onNext(data);
-    console.log(data);
-  });
+  .filter((x)=>x)
+  .withLatestFrom(sourceArrObservable, (key, data) => ({key, data}))
+  .filter(({key, data}) => console.log(key,data) || key === data[0])
+  .map(({data})=>{
+    console.log(data)
+    const shifted = [...data]
+      shifted.shift();
+    return shifted;
+  })
+//
+console.log(source); // source jest
+// a wyskakuje blad, ze nie mozna subskrybowac sie pod undefined
+source.subscribe(({data}) => {
 
+})
+// tak jakby sourca nie bylo
+sourceArrObservable = Rx.Observable.create((observer) => {
+  source.subscribe(({data}) =>{
+    console.log(data)
+    observer.onNext(data)
+  })
+  return function () {
+    console.log('disposed');
+  };
+//  filteredStream$.subscribe(({data}) => observer.onNext(data))
+})
+
+sourceArrObservable.subscribe(observer2)
+
+console.log(sourceArrObservable)

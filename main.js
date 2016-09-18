271f924cb2a0ecab0792955d94314adf7a2355dd
Andrzej <andrzej.fricze@codete.co>
2016-09-18 19:53:05 +0200

    Fix code arrangement

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 import Rx from 'rx';
 import getCharFromKeyCode from './keycodes';
-import { isValue } from './fn';
 import { exampleString } from 'data';
 
-let sourceCharArr = exampleString.split('');
+const sourceCharArr = exampleString.split('');
+
 const arrSubject = new Rx.Subject();
 const fromKeyBoard$ =  Rx.Observable.fromEvent(window, 'keyup');
+
+const source = fromKeyBoard$
+        .map(({keyCode}) => keyCode)
+        .map(keyCode => getCharFromKeyCode(keyCode))
+        .filter((x)=>x)
+        .withLatestFrom(arrSubject, (key, data) => ({key, data}))
+        .filter(({key, data}) => key === data[0])
+        .subscribe(({key, data})=> {
+          console.log('key is ', key);
+          const shifted = [...data];
+          shifted.shift();
+          arrSubject.onNext(shifted);
+          return shifted;
+        });
 
 arrSubject.subscribe((data) => {
   console.log('in subject ' + data)
@@ -14,20 +28,6 @@ arrSubject.subscribe((data) => {
     source.onCompleted();
   }
 });
-
-const source = fromKeyBoard$
-  .map(({keyCode}) => keyCode)
-  .map(keyCode => getCharFromKeyCode(keyCode))
-  .filter((x)=>x)
-  .withLatestFrom(arrSubject, (key, data) => ({key, data}))
-  .filter(({key, data}) => key === data[0])
-  .subscribe(({key, data})=> {
-    console.log('key is ', key);
-    const shifted = [...data];
-    shifted.shift();
-    arrSubject.onNext(shifted);
-    return shifted;
-  });
 
 // INIT
 arrSubject.onNext(sourceCharArr);

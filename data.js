91b5e8aa3061ae4173bf5088e5191829454fc782
Webdwarves <contact@webdwarves.com>
2016-09-21 23:20:37 +0200

    one stream

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-export const exampleString = 'example string fun';
+export const exampleString = 'fun';
--- main.js
+++ main.js
@@ -1,12 +1,54 @@
 import Rx from 'rx';
 import getCharFromKeyCode from './keycodes';
-import { isValue } from './fn';
+import { isValue,identity } from './fn';
 import { exampleString } from 'data';
 
-let sourceCharArr = exampleString.split('');
-const arrSubject = new Rx.Subject();
+const sourceCharArr = exampleString.split('');
 const fromKeyBoard$ =  Rx.Observable.fromEvent(window, 'keyup');
 
+const source = fromKeyBoard$
+  .map(({keyCode}) => keyCode)
+  .map(keyCode => getCharFromKeyCode(keyCode))
+  .filter(identity)
+  .scan((acc, val) =>{
+    const isEqual = acc[0] === val;
+    return acc.slice(+isEqual);
+  },sourceCharArr)
+  .do((acc)=>console.log(acc))
+  .filter((string)=>string.length===0)
+  .subscribe((obj) => {
+    console.log('end');
+    source.onCompleted();
+  });
+
+
+/*
+const arrObservable = new Rx.Observable.just(sourceCharArr).subscribe((observer) => {
+
+    console.log('in arrObservable ' + arrObservable)
+    if(observer.length === 0) {
+      observer.onCompleted();
+    }
+
+    fromKeyBoard$
+      .map(({keyCode}) => keyCode)
+      .map(keyCode => getCharFromKeyCode(keyCode))
+      .filter((x)=>x)
+      .withLatestFrom(arrObservable, (key, data) => ({key, data}))
+      .filter(({key, data}) => key === data[0])
+      .subscribe(({key, data})=> {
+        console.log('key is ', key);
+        const shifted = [...data];
+        shifted.shift();
+        observer.onNext(shifted);
+        return shifted;
+      });
+  }
+
+);
+  */
+
+/*
 arrSubject.subscribe((data) => {
   console.log('in subject ' + data)
   if(data.length === 0) {
@@ -31,3 +73,4 @@ const source = fromKeyBoard$
 
 // INIT
 arrSubject.onNext(sourceCharArr);
+  */

c9375bc6e8abca5598ad7b5de46e661863ecd289
Andrzej <andrzej.fricze@codete.co>
2016-09-26 15:54:37 +0200

    no to lecimy bez ifów

--- main.js
+++ main.js
@@ -5,28 +5,61 @@ import { exampleString } from 'data';
 const sourceCharArr = exampleString.split('');
 const fromKeyBoard$ = Rx.Observable.fromEvent(window, 'keyup');
 
-const newString = string => proper => {
-  const check = ({ true: string.slice(1), false: string });
+Array.prototype.safeReverse = function safeReverse() {
+  return this.slice().reverse();
+}
 
-  return check[proper];
+const left = 'left';
+const right = 'right';
+
+const newString = (toCheck, toView) => (proper, border) => {
+  const key = `${border}_${proper}`;
+
+  const constString = {
+    toCheck,
+    toView,
+    border
+  };
+
+  const check = {
+    'left_false': constString,
+    'right_false': constString,
+    'left_true': {
+      toCheck: toCheck.slice(1).safeReverse(),
+      toView: toView.slice(1),
+      border: right,
+    },
+    'right_true': {
+      toCheck: toCheck.slice(1).safeReverse(),
+      toView: toView.safeReverse().slice(1).safeReverse(),
+      border: left,
+    },
+  };
+
+  return check[key];
 };
 
 const letters$ = fromKeyBoard$
         .startWith('')
         .map(({keyCode}) => getCharFromKeyCode(keyCode))
-        .scan(({toCheck, toView}, val) => {
+        .scan(({toCheck, toView, border}, val) => {
           const properHit = toCheck[0] === val;
-          toCheck = newString(toCheck)(properHit);
 
-          toView = toCheck;
+          const data = newString(toCheck, toView)(properHit, border);
+
+          toCheck = data.toCheck;
+          toView = data.toView;
+          border = data.border;
 
           return {
             toCheck,
             toView,
+            border,
           };
         }, {
           toCheck: sourceCharArr,
           toView: sourceCharArr,
+          border: left
         });
 
 const firstInterval = Number(window.interval.value);
@@ -45,16 +78,12 @@ const lettersGame$ = letters$
 const lettersSubscription = lettersGame$
         .subscribe((x) => {
           textElement.innerText = x.join('');
-        }, x => { alert(x) });
+        }, x => { console.error(x) });
 
-// const finalSubscription = lettersGame$.filter((stringArr) => stringArr.length === 0)
-//         .do(()=>console.log('you won'))
-//         .subscribe(() => {
-//           // closing channel is needed in order to clear timeout (and error that it generates)
-//           finalSubscription.dispose();
-//           lettersSubscription.dispose();
-//         });
-
-Array.prototype.safeReverse = function safeReverse() {
-  return this.slice().reverse();
-}
+const finalSubscription = lettersGame$.filter((stringArr) => stringArr.length === 0)
+        .do(()=>console.log('you won'))
+        .subscribe(() => {
+          // closing channel is needed in order to clear timeout (and error that it generates)
+          finalSubscription.dispose();
+          lettersSubscription.dispose();
+        });

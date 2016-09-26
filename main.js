f50ec8683be4b4ed78c92c8e19076a2eb738977b
Andrzej <andrzej.fricze@codete.co>
2016-09-26 15:24:41 +0200

    havin' fun, aye?

--- main.js
+++ main.js
@@ -5,14 +5,29 @@ import { exampleString } from 'data';
 const sourceCharArr = exampleString.split('');
 const fromKeyBoard$ = Rx.Observable.fromEvent(window, 'keyup');
 
+const newString = string => proper => {
+  const check = ({ true: string.slice(1), false: string });
+
+  return check[proper];
+};
+
 const letters$ = fromKeyBoard$
+        .startWith('')
         .map(({keyCode}) => getCharFromKeyCode(keyCode))
-        .scan((acc, val, index) => {
-          const indexToCompare = +(!(index % 2) && acc.length - 1);
-          const isMatch = (acc[indexToCompare] === val);
-          acc.splice(indexToCompare,isMatch);
-          return acc;
-        }, sourceCharArr);
+        .scan(({toCheck, toView}, val) => {
+          const properHit = toCheck[0] === val;
+          toCheck = newString(toCheck)(properHit);
+
+          toView = toCheck;
+
+          return {
+            toCheck,
+            toView,
+          };
+        }, {
+          toCheck: sourceCharArr,
+          toView: sourceCharArr,
+        });
 
 const firstInterval = Number(window.interval.value);
 
@@ -24,70 +39,22 @@ const textElement = window.text;
 
 const lettersGame$ = letters$
         .withLatestFrom(interval$)
-        .timeout(([_, interval]) => Rx.Observable.timer(interval))
-        .map(([data]) => data);
+        .timeout(([ , interval]) => Rx.Observable.timer(interval))
+        .map(([data]) => data.toView);
 
 const lettersSubscription = lettersGame$
         .subscribe((x) => {
           textElement.innerText = x.join('');
         }, x => { alert(x) });
 
-const finalSubscription = lettersGame$.filter((stringArr) => stringArr.length === 0)
-        .do(()=>console.log('you won'))
-        .subscribe(() => {
-          finalSubscription.dispose(); // closing channel is needed in order to clear timeout (and error that it generates)
-          lettersSubscription.dispose();
-        });
+// const finalSubscription = lettersGame$.filter((stringArr) => stringArr.length === 0)
+//         .do(()=>console.log('you won'))
+//         .subscribe(() => {
+//           // closing channel is needed in order to clear timeout (and error that it generates)
+//           finalSubscription.dispose();
+//           lettersSubscription.dispose();
+//         });
 
-/*
- const arrObservable = new Rx.Observable.just(sourceCharArr).subscribe((observer) => {
-
- console.log('in arrObservable ' + arrObservable)
- if(observer.length === 0) {
- observer.onCompleted();
- }
-
- fromKeyBoard$
- .map(({keyCode}) => keyCode)
- .map(keyCode => getCharFromKeyCode(keyCode))
- .filter((x)=>x)
- .withLatestFrom(arrObservable, (key, data) => ({key, data}))
- .filter(({key, data}) => key === data[0])
- .subscribe(({key, data})=> {
- console.log('key is ', key);
- const shifted = [...data];
- shifted.shift();
- observer.onNext(shifted);
- return shifted;
- });
- }
-
- );
- */
-
-/*
- arrSubject.subscribe((data) => {
- console.log('in subject ' + data)
- if(data.length === 0) {
- arrSubject.onCompleted();
- source.onCompleted();
- }
- });
-
- const source = fromKeyBoard$
- .map(({keyCode}) => keyCode)
- .map(keyCode => getCharFromKeyCode(keyCode))
- .filter((x)=>x)
- .withLatestFrom(arrSubject, (key, data) => ({key, data}))
- .filter(({key, data}) => key === data[0])
- .subscribe(({key, data})=> {
- console.log('key is ', key);
- const shifted = [...data];
- shifted.shift();
- arrSubject.onNext(shifted);
- return shifted;
- });
-
- // INIT
- arrSubject.onNext(sourceCharArr);
- */
+Array.prototype.safeReverse = function safeReverse() {
+  return this.slice().reverse();
+}

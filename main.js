1544083037c9574768ee666b59e497ce350b822e
Andrzej <andrzej.fricze@codete.co>
2016-09-29 20:48:11 +0200

    So freash so clean

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// .takeUntil(Rx.Observable.timer(5000));
-
-// const firstInterval = Number(window.interval.value);
-
-// const interval$ = Rx.Observable.fromEvent(window.interval, 'change')
-//         .pluck('currentTarget', 'value')
-//         .map(e => Number(e))
-//         .startWith(firstInterval);
-
 import Intent from './intent';
 import View from './view';
 import Model from './model';
